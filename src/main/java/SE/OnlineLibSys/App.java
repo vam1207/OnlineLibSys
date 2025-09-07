641c0eef793ede60e86d333b1ef743c3d7aab150
Vamshi <vam.1207@gmail.com>
2025-09-08 01:09:14 +0530

    Added files

---
+++ src/main/java/SE/OnlineLibSys/App.java
@@ -0,0 +1,13 @@
+package SE.OnlineLibSys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Online Library System" );
+    }
+}
